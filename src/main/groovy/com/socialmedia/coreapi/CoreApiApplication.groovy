a56cb3472d2dd39cdba6980442441c225c322388
ciprian <ciprian.nicuta.dev@gmail.com>
2024-01-21 00:58:39 +0200

    ollama snapshot

--- src/main/groovy/com/socialmedia/coreapi/CoreApiApplication.groovy
+++ src/main/groovy/com/socialmedia/coreapi/CoreApiApplication.groovy
@@ -1,12 +1,21 @@
 package com.socialmedia.coreapi
 
+import io.github.amithkoujalgi.ollama4j.core.OllamaAPI
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
-@SpringBootApplication
+//@SpringBootApplication
 class CoreApiApplication {
 
     static void main(String[] args) {
-        SpringApplication.run(CoreApiApplication, args)
+//        SpringApplication.run(CoreApiApplication, args)
+        OllamaAPI ollamaAPI = new OllamaAPI("http://localhost:11434")
+        ollamaAPI.setRequestTimeoutSeconds(120)
+        var callback = ollamaAPI.askAsync("llama2", "say hello")
+        println "yyyyyyy"
+        callback.join()
+        println callback.getResult()
+        println "zzzzzzz"
+
     }
 }
